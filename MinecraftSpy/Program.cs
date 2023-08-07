29c4cf6a9f46b6d75da3474687141debc54c9a35
NickTaSpy <nick61998@windowslive.com>
2023-08-07 03:27:32 +0300

    Added Serilog

--- MinecraftSpy/Program.cs
+++ MinecraftSpy/Program.cs
@@ -4,43 +4,67 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MinecraftSpy;
 using Newtonsoft.Json;
+using Serilog;
+using Serilog.Events;
 
-await Host.CreateDefaultBuilder()
-    .UseConsoleLifetime()
-    .ConfigureServices((hostBuilder, services) =>
-    {
-        services.AddSingleton(hostBuilder.Configuration.GetSection("BotSettings").Get<Settings>() ?? new Settings());
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
 
-        var secrets = JsonConvert.DeserializeObject<AppSecrets>(File.ReadAllText(Constants.FILE_SECRETS));
+try
+{
+    Log.Information("Starting host");
 
-        if (secrets is null)
+    await Host.CreateDefaultBuilder()
+        .UseConsoleLifetime()
+        .ConfigureServices((hostBuilder, services) =>
         {
-            Console.WriteLine("Could not load " + Constants.FILE_SECRETS);
-            Environment.Exit(0);
-        }
+            services.AddSingleton(hostBuilder.Configuration.GetSection("BotSettings").Get<Settings>() ?? new Settings());
 
-        services.AddSingleton(secrets);
+            var secrets = JsonConvert.DeserializeObject<AppSecrets>(File.ReadAllText(Constants.FILE_SECRETS));
 
-        //services.AddHttpClient(Constants.HTTP_CLIENT_PTERODACTYL)
-        //    .ConfigureHttpClient(c =>
-        //    {
-        //        c.BaseAddress = new Uri(Environment.GetEnvironmentVariable(Constants.ENV_PTERODACTYL_ADDRESS) ??
-        //            throw new Exception(Constants.ENV_PTERODACTYL_ADDRESS + " is not available."));
-        //        c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //        c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
-        //            Environment.GetEnvironmentVariable(Constants.ENV_PTERODACTYL_KEY) ??
-        //                throw new Exception(Constants.ENV_PTERODACTYL_KEY + " is not available."));
-        //    });
-        //services.AddSingleton<PterodactylService>();
+            if (secrets is null)
+            {
+                Console.WriteLine("Could not load " + Constants.FILE_SECRETS);
+                Environment.Exit(0);
+            }
 
-        if (string.IsNullOrEmpty(secrets.BotDb))
-        {
-            Console.WriteLine(secrets.BotDb + " hasn't been set.");
-            Environment.Exit(0);
-        }
+            services.AddSingleton(secrets);
 
-        services.AddDbContextFactory<DatabaseContext>(options => options.UseMySql(secrets.BotDb, ServerVersion.AutoDetect(secrets.BotDb)));
+            //services.AddHttpClient(Constants.HTTP_CLIENT_PTERODACTYL)
+            //    .ConfigureHttpClient(c =>
+            //    {
+            //        c.BaseAddress = new Uri(Environment.GetEnvironmentVariable(Constants.ENV_PTERODACTYL_ADDRESS) ??
+            //            throw new Exception(Constants.ENV_PTERODACTYL_ADDRESS + " is not available."));
+            //        c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //        c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+            //            Environment.GetEnvironmentVariable(Constants.ENV_PTERODACTYL_KEY) ??
+            //                throw new Exception(Constants.ENV_PTERODACTYL_KEY + " is not available."));
+            //    });
+            //services.AddSingleton<PterodactylService>();
 
-        services.AddHostedService<BotService>();
-    })
-    .RunConsoleAsync();
+            if (string.IsNullOrEmpty(secrets.BotDb))
+            {
+                Console.WriteLine(secrets.BotDb + " hasn't been set.");
+                Environment.Exit(0);
+            }
+
+            services.AddDbContextFactory<DatabaseContext>(options => options.UseMySql(secrets.BotDb, ServerVersion.AutoDetect(secrets.BotDb)));
+
+            services.AddHostedService<BotService>();
+        })
+        .UseSerilog()
+        .RunConsoleAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
